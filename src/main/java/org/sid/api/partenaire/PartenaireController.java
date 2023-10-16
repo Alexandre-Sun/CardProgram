789f6bfc917479caf250f6d658f4842a499cd63c
HoK-_qN <35311378+CedricLegavre@users.noreply.github.com>
2023-10-16 11:58:13 +0200

    Add files via upload

--- src/main/java/org/sid/api/partenaire/PartenaireController.java
+++ src/main/java/org/sid/api/partenaire/PartenaireController.java
@@ -1,29 +1,39 @@
 package org.sid.api.partenaire;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
 @RequestMapping("/partenaires")
+@Controller
 public class PartenaireController {
 
 	@Autowired
 	private PartenaireService partenaireService;
 
-	@GetMapping("/listPartenaires")
-	public Iterable<Partenaire> getPartenaire() {
-		return partenaireService.getPartenaires();
-	}
+	@Autowired
+	private PartenaireRepository partenaireRepository;
 
 	@GetMapping("/deletePartenaire/{id}")
 	public ModelAndView deletePartenaire(@PathVariable("id") final Long id) {
 		partenaireService.deletePartenaire(id);
 		return new ModelAndView("redirect:/partenaires");
-	}	
-	
-	
+	}
+
+	@GetMapping("/fichePartenaire/{id}")
+	public String afficherPartenaire(@PathVariable Long id, Model model) {
+
+		Optional<Partenaire> part = partenaireRepository.findById(id);
+		Partenaire partenaire = part.get();
+		model.addAttribute("partenaire", partenaire);
+		
+		return "partenaires/fichePartenaire";
+
+	}
 }
